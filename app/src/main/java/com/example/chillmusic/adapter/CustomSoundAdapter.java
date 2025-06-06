418e83e7b98b10adb34dd0dc211a86a1cedbd4dd
Lưu Viết Công <congluu090502@gmail.com>
2025-06-06 15:29:48 +0700

    Update for app

--- app/src/main/java/com/example/chillmusic/adapter/CustomSoundAdapter.java
+++ app/src/main/java/com/example/chillmusic/adapter/CustomSoundAdapter.java
@@ -72,7 +72,7 @@ public class CustomSoundAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
             viewHolder.tvLabel.setText(item.getName());
 
             // Xử lý URL ảnh
-            final String baseUrl = "http://10.0.2.2:5000";
+            final String baseUrl = "http://10.0.2.2:3000";
             String imageUrl = item.getImageUrl();
             String imageToLoad = null;
             if (imageUrl != null && !imageUrl.isEmpty()) {
